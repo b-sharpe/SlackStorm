267464317c7a9b8b515771e0e1332329fde1dc46
Clément Garbay <clementgarbay@gmail.com>
2016-01-16 15:36:56 +0100

    Allow removing of individual channels

--- src/com/bryansharpe/slackstorm/SlackSettings.java
+++ src/com/bryansharpe/slackstorm/SlackSettings.java
@@ -7,26 +7,25 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.IconLoader;
-import org.jdesktop.swingx.action.ActionManager;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.HashSet;
 
 /**
  * Created by bsharpe on 11/2/2015.
+ * Updated by Clément GARBAY "clementgarbay@gmail.com" on 01/16/2016
  *
  * Create the main toolbar group with add/clear
- *
- * @TODO: allow removing of individual channels
  */
 public class SlackSettings extends ActionGroup {
     @NotNull
     @Override
     public AnAction[] getChildren(AnActionEvent anActionEvent) {
-        final AnAction[] children = new AnAction[2];
+        final AnAction[] children = new AnAction[3];
 
         children[0] = new addChannel();
-        children[1] = new removeChannels();
+        if (SlackStorage.getInstance().settings.size() > 0) {
+            children[1] = new removeChannel();
+            children[2] = new removeChannels();
+        }
 
         return children;
     }
@@ -85,6 +84,42 @@ public class SlackSettings extends ActionGroup {
             }
         }
     }
+
+    /**
+     * Clear a channel from settings
+     */
+    public class removeChannel extends AnAction {
+        public removeChannel() {
+            super("Remove Slack Channel");
+        }
+        @Override
+        public void actionPerformed(AnActionEvent e) {
+            final Project project = e.getData(CommonDataKeys.PROJECT);
+
+            SlackStorage slackStorage = SlackStorage.getInstance();
+
+            String[] channelsName = slackStorage.settings.keySet().toArray(new String[slackStorage.settings.size()]);
+
+            if (channelsName.length > 0) {
+                String channelToRemove = Messages.showEditableChooseDialog(
+                    "Select the channel to remove",
+                    "Slack Settings",
+                    IconLoader.getIcon("/icons/slack.png"),
+                    channelsName,
+                    channelsName[0],
+                    null
+                );
+
+                if (channelToRemove != null) {
+                    slackStorage.settings.remove(channelToRemove);
+                    slackStorage.aliases.remove(channelToRemove);
+
+                    Messages.showMessageDialog(project, "Channel \"" + channelToRemove + "\" removed.", "Information", Messages.getInformationIcon());
+                }
+            }
+        }
+    }
+
 
     /**
      * Clear all channels from settings
